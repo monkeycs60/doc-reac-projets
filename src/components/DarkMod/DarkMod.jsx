03fe566ed49218be60dd0ab6a70864c300598b69
monkeycs60 <clement.serizay@gmail.com>
2023-01-16 02:05:44 +0100

    finalisation dark mod

--- src/components/DarkMod/DarkMod.jsx
+++ src/components/DarkMod/DarkMod.jsx
@@ -1,24 +1,28 @@
 import React, { useContext, useEffect } from "react";
 import "./DarkMod.css";
-import DarkMoon from "../../assets/moon-solid.svg";
+import LightMoon from "../../assets/moon-solid.svg";
 import { DarkModContext } from "../../main";
-import LightMoon from "../../assets/moon-regular.svg";
-import "../../App.css"
+import DarkMoon from "../../assets/moon-regular.svg";
+import "../../App.css";
 
 const DarkMod = () => {
   const { darkMode, setDarkMode } = useContext(DarkModContext);
 
   const handleClick = () => {
     setDarkMode(!darkMode);
-};
+  };
 
-useEffect(() => {
-  darkMode
-    ? 
-        document.body.classList.add("darker")
-    : 
-        document.body.classList.remove("darker");
-}, [darkMode]);
+  useEffect(() => {
+    darkMode
+      ? (document.body.classList.add("darker"),
+        document.getElementById("DarkButton").classList.add("button-bg"),
+        document.getElementById("darkImage").src = LightMoon
+        )
+      : (document.body.classList.remove("darker"),
+        document.getElementById("DarkButton").classList.remove("button-bg"),
+        document.getElementById("darkImage").src = DarkMoon
+        );
+  }, [darkMode]);
 
   return (
     <button type="button" id="DarkButton" onClick={handleClick}>
--- src/pages/aboutpage/AboutPage.jsx
+++ src/pages/aboutpage/AboutPage.jsx
@@ -4,7 +4,7 @@ import "./AboutPage.css";
 
 const AboutPage = () => {
   return (
-    <div className="darker">
+    <div>
       <h1>About Claude Dark</h1>
       <p>
         Claude Dark is an average computer guy with a passion for technology and
